dbf24da412bac1b74d8fd33ac624d8652462a893
instyo <nsetyo19@gmail.com>
2025-09-14 18:45:44 +0700

    Initial Commit

---
+++ SwiftUI Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI Chat
+//
+//  Created by ikhwan on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
